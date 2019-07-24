402ca4207eb9fdb30591dd6f6ca7fe64f4b0cea9
John Garbutt <john@johngarbutt.com>
2019-07-24 13:59:15 +0100

    Hack in a debug mode

--- prometheus-exporter.py
+++ prometheus-exporter.py
@@ -9,17 +9,24 @@ from prometheus_client.core import GaugeMetricFamily, CounterMetricFamily, REGIS
 from prometheus_client import start_http_server
 
 
-def get_usage(cloud, project_id):
-    url = "/os-simple-tenant-usage/%s?start=2019-04-01T00:00:00&end=2019-06-26T00:00:00"
-    response = cloud.compute.get(url % project_id)
-    usages = response.json()['tenant_usage']
-    usages['server_usage_count'] = len(usages.get('server_usages', []))
-    usages['server_usages'] = None
-    del usages['server_usages']
-    usages['total_memory_mb_usage'] = int(usages.get('total_memory_mb_usage',0))
-    usages['total_vcpus_usage'] = int(usages.get('total_vcpus_usage',0))
-    usages['total_iris_vcpus_usage'] = int(usages.get('total_vcpus_usage',0))
-    return usages
+def get_usage_for_project(cloud, project):
+    q2 = "start=2019-04-01T00:00:00&end=2019-07-01T00:00:00"
+    url = "/os-simple-tenant-usage/%s?%s"
+    response = cloud.compute.get(url % project.id, q2)
+    raw_usage = response.json()['tenant_usage']
+
+    usage = {'project_id': project.id, 'project_name': project.name}
+    usage['server_usage_count'] = len(raw_usage.get('server_usages', []))
+    usage['total_memory_mb_usage'] = int(raw_usage.get('total_memory_mb_usage',0))
+    usage['total_vcpus_usage'] = int(raw_usage.get('total_vcpus_usage',0))
+    usage['total_iris_vcpus_usage'] = int(raw_usage.get('total_vcpus_usage',0))
+    return usage
+
+
+def get_usage(cloud):
+    for project in cloud.identity.projects():
+        yield get_usage(cloud, project)
+
 
 class CustomCollector(object):
     def __init__(self, cloud):
@@ -28,17 +35,18 @@ class CustomCollector(object):
     def collect(self):
         #yield GaugeMetricFamily('my_gauge', 'Help text', value=7)
 	
-        vcpu = CounterMetricFamily('openstack_vcpu_usage', 'Help text', labels=['project', 'project_uuid'])
-        ram_mb = CounterMetricFamily('openstack_ram_mb_usage', 'Help text', labels=['project'])
-        instances = CounterMetricFamily('openstack_instances_usage', 'Help text', labels=['project'])
+        vcpu = CounterMetricFamily('openstack_vcpu_usage',
+                'Help text', labels=['project'])
+        ram_mb = CounterMetricFamily('openstack_ram_mb_usage',
+                'Help text', labels=['project'])
+        instances = CounterMetricFamily('openstack_instances_usage',
+                'Help text', labels=['project'])
 
-	#total_memory_mb_usage': 22964517.546666667, u'total_vcpus_usage'
-
-	for project in cloud.identity.projects():
-            usage = get_usage(cloud, project.id)
-
-            instances.add_metric([project.name], usage['server_usage_count'])
-            vcpu.add_metric([project.name, project.id, "vcpu"], usage.get('total_vcpus_usage'))
+        usages = get_usage()
+	for usage in usages:
+            project_name = usage['project_name']
+            instances.add_metric([project_name], usage['server_usage_count'])
+            vcpu.add_metric([project_name, "vcpu"], usage.get('total_vcpus_usage'))
             ram_mb.add_metric([project.name], usage.get('total_memory_mb_usage'))
 
         yield vcpu
@@ -46,13 +54,10 @@ class CustomCollector(object):
         yield instances
 
 if __name__ == '__main__':
-  # connect to openstack
-  cloud = openstack.connect()
-  # check connection
-  cloud.compute.servers()
+    cloud = openstack.connect()
+    cloud.compute.servers()
+    print list(get_usage(cloud))
 
-  REGISTRY.register(CustomCollector(cloud))
-
-  start_http_server(8000)
-  while True: time.sleep(1)
-
+    #REGISTRY.register(CustomCollector(cloud))
+    #start_http_server(8000)
+    #while True: time.sleep(1)
