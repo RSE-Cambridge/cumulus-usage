0ed686b2000b2c1dc8c5b91644dd20f91b09b700
John Garbutt <johng@stackhpc.com>
2019-09-03 09:31:10 +0000

    Change units to phyiscal and vcpus

--- prometheus-exporter.py
+++ prometheus-exporter.py
@@ -1,5 +1,6 @@
 #!/bin/python
 
+import calendar
 import json
 import time
 
@@ -60,18 +61,23 @@ class CustomCollector(object):
         yield ram_mb
         yield instances
 
-def get_month(month):
-    return "start=2019-%02d-01T00:00:00&end=2019-%02d-01T00:00:00" % (month, month + 1)
+def get_month(year, month):
+    month_int = month_to_int[month]
+    return "start=%s-%02d-01T00:00:00&end=%s-%02d-01T00:00:00" % (year, month_int, year, month_int + 1)
+
+month_to_int = {month.lower(): i for i,month in enumerate(calendar.month_name) if month != ""}
+month_days = {month: calendar.monthrange(2019, i)[1] for month, i in month_to_int.items()}
 
 def get_months():
-    months = [
-      ("april", get_month(4)),
-      ("may", get_month(5)),
-      ("june", get_month(6)),
-      ("july", get_month(7)),
-      ("august", get_month(8)),
+    months_raw = [
+	(2019, "april"),
+	(2019, "may"),
+	(2019, "june"),
+	(2019, "july"),
+	(2019, "august"),
+	(2019, "september"),
     ]
-    return months
+    return [(month, get_month(year, month)) for year, month in months_raw]
 
 def get_weeks():
     import datetime
@@ -94,36 +100,36 @@ if __name__ == '__main__':
     cloud = openstack.connect()
     cloud.compute.servers()
 
-    monthly_usages = get_usages(cloud, get_months())
-    weekly_usages = get_usages(cloud, list(get_weeks()))
-    all_usages = list(monthly_usages) + list(weekly_usages)
+    #monthly_usages = get_usages(cloud, get_months())
+    #weekly_usages = get_usages(cloud, list(get_weeks()))
+    #all_usages = list(monthly_usages) + list(weekly_usages)
+    all_usages = get_usages(cloud, get_months())
 
     print ("project_name, timeframe, "
-           "average standard cores, standard core hours, average physical hypervisors, average physical cores, "
+           "average physical cores, physical core hours, average physical hypervisors, average vcpus, "
            "active vm count, vm memory usage hours, vm core usage hours, project_id")
 
     for month, usage in all_usages:
-        days = 7
-        if (month == "april" or month == "june"):
-            days = 30
-        if not month.startswith("wb"):
-            days = 31
-
-        hypervisor_hours = float(usage['total_vcpus_usage']) / 56.0  # 56 vCPU per hypervisor
-        physical_core_hours = hypervisor_hours * 32  # 32 physical_cores in a hypervisor
-        adjusted_core_hours = physical_core_hours * 1.171875
+	days = month_days.get(month, 7)
         hours = 24 * days
-        average_cores = physical_core_hours / hours
-        adjusted_average_cores = average_cores * 1.171875
+
+	vcpu_hours = float(usage['total_vcpus_usage'])
+	average_vcpus = vcpu_hours / hours
+
+        hypervisor_hours = vcpu_hours / 56.0  # 56 vCPU per hypervisor
         average_hypervisors = hypervisor_hours / hours
+
+        physical_core_hours = hypervisor_hours * 32  # 32 physical_cores in a hypervisor
+        #adjusted_core_hours = physical_core_hours * 1.171875
+        average_physical_cores = physical_core_hours / hours
 
 	usage_list = [
              usage['project_name'],
 	     month.strip("wb:"),
-             str(adjusted_average_cores),
-             str(adjusted_core_hours),
+             str(average_physical_cores),
+             str(physical_core_hours),
              str(average_hypervisors),
-             str(average_cores),
+             str(average_vcpus),
 	     str(usage['server_usage_count']),
 	     str(usage['total_memory_mb_usage']),
 	     str(usage['total_vcpus_usage']),
